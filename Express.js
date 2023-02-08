bec572c0e2eb1c6d953869f4422f3011668e8859
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-02-08 23:05:09 +0530

    Update Express.js

--- Express.js
+++ Express.js
@@ -1,6 +1,43 @@
-function show(){
-    var argArray = Array.prototype.slice.call(arguments);
-    arguArray.sort();
-    console.log(argArray)
-}
-show(2,15,6)
+//basic template for express.js
+
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("hello World with fun")
+});
+
+app.get('/about',(req,res)=>{
+    res.send("hello world from about")
+})
+
+
+app.listen(8000,()=>{
+    console.log("listing the port at 8000")
+})
+
+
+
+
+//routing in express.js
+const express = require("express");
+const app = express();
+const port=3000;
+
+app.get("/",(req,res)=>{
+    res.send("welcome to my home page")
+})
+
+app.get("/about",(req,res)=>{
+    res.status(200).send("welcome to my about page")
+})
+
+app.get("/contact",(req,res)=>{
+    res.status(200).send("welcome to my contact page")
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`listinig to the port no ${port}`)
+})
